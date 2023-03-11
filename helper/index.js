1d8530ef4080ea99773f6e293ef913815a47dfaf
Samitha <samitha@Samithas-MacBook-Pro---Calcey.local>
2023-03-11 23:39:14 +0530

    refactor: reduce complexity

--- helper/index.js
+++ helper/index.js
@@ -1,25 +1,17 @@
 const checkForValidity = (node, componentName, dependOn, propName) => {
-    // Return whether the validity is a success or not.
     const cComp = node.name;
     const cCompAttr = node.attributes;
-    if (cComp.name === componentName) {
-        if (cCompAttr.some(attr => attr.name && attr.name.name === 'ignoreESLintPropValidation')) {
-            return true;
-        }
-        if (dependOn) {
-            const dependable = cCompAttr.some(attr => attr.name && attr.name.name === dependOn);
-            if (dependable && cCompAttr.every(attr => attr.name && attr.name.name !== propName)) {
-                return false;
-            }
-            return true;
-        } else {
-            if (cCompAttr.some(attr => attr.name && attr.name.name === propName)) {
-                return true;
-            }
-            return false;
-        }
+    
+    if (cComp.name !== componentName) return true;
+    
+    if (cCompAttr.some(attr => attr.name && attr.name.name === 'ignoreESLintPropValidation')) return true;
+    
+    if (dependOn) {
+        const dependable = cCompAttr.some(attr => attr.name && attr.name.name === dependOn);
+        return !dependable || cCompAttr.some(attr => attr.name && attr.name.name === propName);
+    } else {
+        return cCompAttr.some(attr => attr.name && attr.name.name === propName);
     }
-    return true;
 };
 
 module.exports = { checkForValidity };
